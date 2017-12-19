60449d1cbff64a032347f253ce64f4252ef7ae9e
djunh1 <djunh1@gmail.com>
2017-12-19 16:54:57 -0500

    add games API for individual games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -37,6 +37,39 @@ class Api::V1::GamesController < ApplicationController
       is_success: true
     }, status: :ok
 
+  end
+
+  def show
+    game = Game.find(params[:id])
+
+    today = Date.today
+    reservations = Reservation.where(
+      "game_id = ? AND start_date >= ? AND status = ?",
+      params[:id], today, 1
+    )
+    #transform reservations we get from DB
+    unavailable_dates = reservations.map { |r|
+      (r[:start_date].to_datetime..r[:start_date].to_datetime).map{ |day| day.strftime("%Y-%m-%d") }
+    }.flatten.to_set
+
+    #gets only the day via pluck
+    calendars = Calendar.where(
+      "game_id = ? AND status = ? and day >= ?",
+      params[:id], 1, today
+      ).pluck(:day).map(&:to_datetime).map { |day| day.strftime("%Y-%m-%d") }.flatten.to_set
+
+    unavailable_dates.merge calendars
+
+    if !game.nil?
+      game_serializer = GameSerializer.new(
+        game,
+        image: game.cover_photo('medium'),
+        unavailable_dates: unavailable_dates
+      )
+      render json: {game: game_serializer, is_success: true}, status: :ok
+    else
+      render json: {error: 'Invalid game id', is_success: false}, status: 422
+    end
 
   end
 end
